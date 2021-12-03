f37bc541e268ca218917972f1077c3281f09d766
TrungHieu175 <ablohaha01@gmail.com>
2021-12-04 06:49:28 +0700

    update

---
+++ Module 2/ss1_Introduction to Java/ThucHanh/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;
+        double d = 3.0;
+        float f = 5.0f;
+        char c = 'a';
+        String s = "Hà Nội";
+        System.out.println("i = " + i);
+        System.out.println("d = " + d);
+        System.out.println("f = " + f);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+    }
+}
---
+++ Module 2/ss1_Introduction to Java/operator-expression/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        float width;
+        float height;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter width: ");
+        width = scanner.nextFloat();
+
+        System.out.print("Enter height: ");
+        height = scanner.nextFloat();
+        float area = width * height;
+
+        System.out.println("Area is: " + area);
+    }
+}
